bc910ce62a3bb1cfd0c0b9d896044a4cd795d0b1
sneezy-5 <adingranarcisse2@gmail.com>
2022-12-10 16:51:43 +0000

    mdif du iframe facebook

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -163,7 +163,8 @@ L'ONG sourire d'Afrique est aussi en charge d' accompagner les jeunes et les fem
 
       </div>
        <div class="col-md-6 container-fluid m-2">
-       <iframe class="container-fluid col-md" src="https://web.facebook.com/profile.php?id=100086654464545"></iframe>
+       <iframe class="container-fluid col-md" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fweb.facebook.com%2Fprofile.php%3Fid%3D100086654464545&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+  
       </div>
   </div>
 
